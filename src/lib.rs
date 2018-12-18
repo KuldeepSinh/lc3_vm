ff5bb922fcce72b47fd4c79fab3468defdf92a57
Kuldeep Sinh Chauhan <kuldeep.sinh@orionhealth.com>
2018-12-18 16:08:37 -0700

    Added run function as an entry point to handle instruction processing.

--- src/lib.rs
+++ src/lib.rs
@@ -1,54 +1,58 @@
 pub mod hardware;
-// use crate::hardware::Memory;
-// use crate::hardware::OpCode;
-// use crate::hardware::PC_START;
-//
-// fn run(memory: Memory) {
-//     //loop through memory from PC_START to end of the memory.
-//     let mut pc_pointer = PC_START as usize;
-//     loop {
-//         //get instruction from the current position
-//         let instruction = memory.instructions[pc_pointer];
-//         //extract op_code and run operation...
-//         match_and_perform_op(&instruction);
-//         //increment pointer
-//         pc_pointer += 1;
-//     }
-// }
+use self::hardware::memory::Memory;
+use self::hardware::opcode::OpCode;
+use self::hardware::register::PC_START;
 
-// fn match_and_perform_op(instruction: &u16) {
-//     //extract op_code from the instruction
-//     let op_code = extract_op_code(&instruction);
-//     //match and perform operation
-//     match op_code {
-//         Some(OpCode::op_add) => {}
-//         // Some(OpCode::op_and) => {}
-//         // Some(OpCode::op_not) => {}
-//         // Some(OpCode::op_br) => {}
-//         // Some(OpCode::op_jmp) => {}
-//         // Some(OpCode::op_jsr) => {}
-//         // Some(OpCode::op_ld) => {}
-//         // Some(OpCode::op_ldi) => {}
-//         // Some(OpCode::op_ldr) => {}
-//         // Some(OpCode::op_lea) => {}
-//         // Some(OpCode::op_st) => {}
-//         // Some(OpCode::op_sti) => {}
-//         // Some(OpCode::op_str) => {}
-//         Some(OpCode::op_trap) => {}
-//         _ => {}
-//     }
-// }
-//
-// fn extract_op_code(instruction: &u16) -> Option<OpCode> {
-//     OpCode::get_op_code(instruction >> 12)
-// }
-//
-// #[cfg(test)]
-// mod extract_op_code_test {
-//     use super::*;
-//     #[test]
-//     fn extract_test() {
-//         let four = 16384;
-//         assert_eq!(Some(OpCode::op_jsr), extract_op_code(&four));
-//     }
-// }
+pub fn run(memory: Memory) {
+    //loop through memory from PC_START to end of the memory.
+    let mut pc_pointer = PC_START as usize;
+    loop {
+        //get instruction from the current position
+        let instruction = memory.cells[pc_pointer];
+        //extract op_code and run operation...
+        match_and_perform_op(&instruction);
+        //increment pointer
+        pc_pointer += 1;
+    }
+}
+
+fn match_and_perform_op(instruction: &u16) {
+    //extract op_code from the instruction
+    let op_code = extract_op_code(&instruction);
+    //match and perform operation
+    match op_code {
+        Some(OpCode::OpAdd) => {}
+        // Some(OpCode::op_and) => {}
+        // Some(OpCode::op_not) => {}
+        // Some(OpCode::op_br) => {}
+        // Some(OpCode::op_jmp) => {}
+        // Some(OpCode::op_jsr) => {}
+        // Some(OpCode::op_ld) => {}
+        // Some(OpCode::op_ldi) => {}
+        // Some(OpCode::op_ldr) => {}
+        // Some(OpCode::op_lea) => {}
+        // Some(OpCode::op_st) => {}
+        // Some(OpCode::op_sti) => {}
+        // Some(OpCode::op_str) => {}
+        //Some(OpCode::OpTrap) => {}
+        _ => {}
+    }
+}
+
+//Each instruction is 16 bits long, with the left 4 bits storing the opcode.
+//The rest of the bits are used to store the parameters.
+//To extract left 4 bits out of the instruction, we'll use ">>" shift-right
+//operator and shift first 4 bits 12 positions towards right
+fn extract_op_code(instruction: &u16) -> Option<OpCode> {
+    OpCode::get_op_code(instruction >> 12)
+}
+
+#[cfg(test)]
+mod extract_op_code_test {
+    use super::*;
+    #[test]
+    fn extract_test() {
+        let four = 16384;
+        assert_eq!(Some(OpCode::OpJsr), extract_op_code(&four));
+    }
+}
