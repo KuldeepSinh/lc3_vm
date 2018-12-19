07c687194b17bab360f2696a383599d6461cce86
Kuldeep Sinh Chauhan <kuldeep.sinh@orionhealth.com>
2018-12-18 19:12:34 -0700

    Added doc comments.

--- src/hardware/mod.rs
+++ src/hardware/mod.rs
@@ -1,3 +1,7 @@
+//! # Hardware - A Crate to simulate hardware of LC-3 (Little Computer - 3).
+//!
+//! The `hardware` create is created to simulate hardware components of the LC-3 (Little Computer - 3) CPU.
+//! Main components of the CPU are Memory, Registers and OpCodes.
 pub mod memory;
 pub mod opcode;
 pub mod register;
--- src/hardware/register.rs
+++ src/hardware/register.rs
@@ -1,21 +1,51 @@
+//! A `register` is a slot for storing a single value on the CPU.
+//! Registers are like the "workbench" of the CPU.
+//! For the CPU to work with a piece of data, it has to be in one of the registers.
+//! However, since there are just a few registers, only a minimal amount of data can be loaded at any given time.
+//! Programs work around this by loading values from memory into registers, calculating values into other registers,
+//! and then storing the final results back in memory.
 //set initial value of the program counter = 0x3000
 pub const PC_START: u16 = 0x3000;
+
+/// The LC-3 has 10 total registers, each of which is 16 bits. Most of them are general purpose, but a few have designated roles.
+///
+/// - 8 general purpose registers (R0-R7)
+/// - 1 program counter (PC) register
+/// - 1 condition flags (COND) register
+/// The general purpose registers can be used to perform any program calculations.
+/// The program counter is an unsigned integer which is the address of the next instruction in memory to execute.
+/// The condition flags tell us information about the previous calculation.
 #[derive(Debug)]
 pub struct Registers {
-    pub r_01: u16,   // general purpose register
-    pub r_02: u16,   // general purpose register
-    pub r_03: u16,   // general purpose register
-    pub r_04: u16,   // general purpose register
-    pub r_05: u16,   // general purpose register
-    pub r_06: u16,   // general purpose register
-    pub r_07: u16,   // general purpose register
-    pub r_pc: u16,   // program counter
+    /// `r_00` is a general purpose register.
+    pub r_00: u16, // general purpose register
+    /// `r_01` is a general purpose register.
+    pub r_01: u16, // general purpose register
+    /// `r_02` is a general purpose register.
+    pub r_02: u16, // general purpose register
+    /// `r_03` is a general purpose register.
+    pub r_03: u16, // general purpose register
+    /// `r_04` is a general purpose register.
+    pub r_04: u16, // general purpose register
+    /// `r_05` is a general purpose register.
+    pub r_05: u16, // general purpose register
+    /// `r_06` is a general purpose register.
+    pub r_06: u16, // general purpose register
+    /// `r_07` is a general purpose register.
+    pub r_07: u16, // general purpose register
+    /// `r_pc` is a register for program counter.
+    pub r_pc: u16, // program counter
+    /// `r_cond` is a register to store cinformation about the previous calculation.
     pub r_cond: u16, // condition flag
 }
 
 impl Registers {
+    /// `new()` is an associated method of `Registers`.
+    /// It is used to initialize all the registers to zero, except
+    /// `r_pc`, which represents the program counter. It's initialized with value = `0x3000`.
     pub fn new() -> Registers {
         Registers {
+            r_00: 0,        // general purpose register
             r_01: 0,        // general purpose register
             r_02: 0,        // general purpose register
             r_03: 0,        // general purpose register
@@ -29,22 +59,34 @@ impl Registers {
     }
 }
 
-// The R_COND register stores condition flags which provide information
-// about the most recently executed calculation.
-// This allows programs to check logical conditions such as if (x > 0) { ... }.
-// The LC-3 uses only 3 condition flags
-// which indicate the sign of the previous calculation.
+/// The R_COND register stores condition flags which provide information
+/// about the most recently executed calculation.
+/// This allows programs to check logical conditions such as if (x > 0) { ... }.
+/// The LC-3 uses only 3 condition flags
+/// which indicate the sign of the previous calculation.
+/// - `FlPos` represents `Positive` value.
+/// - `FlZro` represents `Zero` value.
+/// - `FlNeg` represents `Negative` value.
 pub enum ConditionFlag {
+    /// `ConditionFlag::FlPos` represents `Positive`.
     FlPos, // = 1, Positive
+    /// `ConditionFlag::FlZro` represents `Zero`.
     FlZro, // = 2, Zero
+    /// `ConditionFlag::FlNeg` represents `Negative`.
     FlNeg, // = 4, Negative
 }
 
-pub fn get_flag_value(flag: ConditionFlag) -> u8 {
-    match flag {
-        ConditionFlag::FlPos => 1 << 0, // Positive
-        ConditionFlag::FlZro => 1 << 1, // Zero
-        ConditionFlag::FlNeg => 1 << 2, // Negative
+impl ConditionFlag {
+    /// `get_flag_value` function returns `u8` value for the `ConditionFlag` passed to its argument.
+    /// - `ConditionFlag::FlPos` = 1, Positive
+    /// - `ConditionFlag::FlZro` = 2, Zero
+    /// - `ConditionFlag::FlNeg` = 4, Negative
+    pub fn get_flag_value(flag: ConditionFlag) -> u8 {
+        match flag {
+            ConditionFlag::FlPos => 1 << 0, // Positive
+            ConditionFlag::FlZro => 1 << 1, // Zero
+            ConditionFlag::FlNeg => 1 << 2, // Negative
+        }
     }
 }
 
@@ -63,15 +105,15 @@ mod condition_flag_test {
     use super::*;
     #[test]
     fn value_of_flpos_should_be_1() {
-        assert_eq!(1, get_flag_value(ConditionFlag::FlPos));
+        assert_eq!(1, ConditionFlag::get_flag_value(ConditionFlag::FlPos));
     }
     #[test]
     fn value_of_flzro_should_be_2() {
-        assert_eq!(2, get_flag_value(ConditionFlag::FlZro));
+        assert_eq!(2, ConditionFlag::get_flag_value(ConditionFlag::FlZro));
     }
 
     #[test]
     fn value_of_flneg_should_be_4() {
-        assert_eq!(4, get_flag_value(ConditionFlag::FlNeg));
+        assert_eq!(4, ConditionFlag::get_flag_value(ConditionFlag::FlNeg));
     }
 }
