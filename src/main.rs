de5c323aea1b329e0523bb5206644c67535e8fa0
Kuldeep Sinh Chauhan <kuldeep.sinh@orionhealth.com>
2018-12-24 09:36:48 -0700

    Removed caching of Termios. Handled the same with direct fn calls.

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,16 @@ fn main() -> Result<(), Box<Error>> {
     let signals = Signals::new(&[SIGINT])?;
     thread::spawn(move || {
         for _sig in signals.forever() {
+            //Interrupt (Ctrl + C) is handled as follows...
+            //Terminal is restored to its original configuration
+            //Process is exited with (-2)
             lc3_vm::terminal::handle_interrupt();
         }
     });
+    //change terminal as follows...
+    //1. turn off canonical mode
+    //2. turn off echo mod
+    lc3_vm::terminal::change_terminal();
     println!("Hello, world!");
     Ok(())
 }
