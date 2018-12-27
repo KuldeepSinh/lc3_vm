cee03301e940d088698a89f40995433a21ffae23
Kuldeep Sinh Chauhan <kuldeep.sinh@orionhealth.com>
2018-12-27 14:27:12 -0700

    updated use statements in main, so that to be more specific about fn calling.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
 extern crate signal_hook;
 
 use lc3_vm;
-use lc3_vm::hardware::instructions::*;
-use lc3_vm::sys::terminal::*;
+use lc3_vm::hardware::instructions;
+use lc3_vm::sys::terminal;
 use std::env;
 use std::error::Error;
 use std::process;
 
 fn main() -> Result<(), Box<Error>> {
     //setup for interrupt handling.
-    spawn_thread_for_signal_processing()?;
+    terminal::spawn_thread_for_signal_processing()?;
     //handle command line arguments and process instructions
     match lc3_vm::handle_args(env::args()) {
         Ok(mem) => {
-            process_instructions(mem);
+            instructions::process_instructions(mem);
             //restore terminal settings
-            restore_terminal_settings();
+            terminal::restore_terminal_settings();
             //return
             Ok(())
         }
         Err(_) => {
             //restore terminal settings
-            restore_terminal_settings();
+            terminal::restore_terminal_settings();
             //exit
             process::exit(1)
         }
