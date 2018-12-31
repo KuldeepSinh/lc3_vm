c2456061d32657217fd1f1d572c60ec4953d4347
Kuldeep Sinh Chauhan <kuldeep.sinh@orionhealth.com>
2018-12-31 10:47:50 -0700

    Added doc comments for sign_extend fn

--- src/hardware/instructions/mod.rs
+++ src/hardware/instructions/mod.rs
@@ -14,6 +14,7 @@ pub mod sti;
 pub mod str;
 pub mod trap;
 
+/// `sign_extend` is used to convert a 5 bit number into an equivalent 16 bit number.
 fn sign_extend(mut x: u16, bit_count: u8) -> u16 {
     if (x >> (bit_count - 1)) & 1 != 0 {
         x |= 0xFFFF << bit_count;
